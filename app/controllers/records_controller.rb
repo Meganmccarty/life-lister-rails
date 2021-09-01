80a6b42747cc248cc9a5220cb013ffaa11b5f0cc
Megan McCarty <megan_mccarty@hotmail.com>
2021-09-01 14:48:45 -0400

    Add index and create methods for records

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,2 +1,17 @@
 class RecordsController < ApplicationController
+
+    def index
+        records = Record.all
+        render json: records
+    end
+
+    def create
+        record = Record.create!(record_params)
+        render json: record, status: :created
+    end
+
+    def record_params
+        params.permit(:species, :common_name, :image, :date)
+    end
+
 end
