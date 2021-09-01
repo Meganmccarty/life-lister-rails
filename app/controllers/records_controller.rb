9c8bc80b79f77548358a89343299cbd95e8c83b1
Megan McCarty <megan_mccarty@hotmail.com>
2021-09-01 14:49:35 -0400

    Add methods for error handling

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,4 +1,6 @@
 class RecordsController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
     def index
         records = Record.all
@@ -12,6 +14,16 @@ class RecordsController < ApplicationController
 
     def record_params
         params.permit(:species, :common_name, :image, :date)
+    end
+
+    private
+
+    def render_not_found_response
+        render json: { error: "Record not found" }, status: :not_found
+    end
+
+    def render_unprocessable_entity_response(exception)
+        render json: { errors: exception.record.errors.full_messages }, status: :unprocessable_entity
     end
 
 end
