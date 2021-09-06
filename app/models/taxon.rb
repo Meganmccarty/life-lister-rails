3eed26d46f06fa6863ef3901ad3cd7de1bab0fcb
Megan McCarty <megan_mccarty@hotmail.com>
2021-09-06 12:23:42 -0400

    Create custom method

--- app/models/taxon.rb
+++ app/models/taxon.rb
@@ -1,4 +1,13 @@
 class Taxon < ApplicationRecord
     has_many :records
     has_many :users, through: :records
+
+    class << self
+        def taxon_from_api(result)
+            self.find_or_create_by(
+                species: result[0].name,
+                common_name: result[0].preferred_common_name
+            ) 
+        end
+    end
 end
