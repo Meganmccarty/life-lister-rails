b98089259fafa00e3309abe9568af7b39f123a1f
Megan McCarty <megan_mccarty@hotmail.com>
2021-09-06 12:48:46 -0400

    Change user routes, add show mand authorize methods, alter create method

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
     resources :records, only: [:index, :create, :update, :destroy]
     resources :taxons, only: [:index]
-    resources :users, only: [:create]
+    post "/signup", to: "users#create"
+    get "/profile", to: "users#show"
 end
