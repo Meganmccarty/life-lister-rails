06e891a6a9330efc4e2fbd1c7b35fd23398c7ea7
Megan McCarty <megan_mccarty@hotmail.com>
2021-09-06 12:43:43 -0400

    Add create method and private methods for users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,21 @@
 class UsersController < ApplicationController
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+
+    def create
+        user = User.create!(user_params)
+        if user.valid?
+            render json: user, status: :created
+        end
+    end
+
+    private
+
+    def user_params
+        params.permit(:username, :password, :password_confirmation)
+    end
+
+    def render_unprocessable_entity_response(exception)
+        render json: { errors: exception.users.errors.full_messages }, status: :unprocessable_entity
+    end
+
 end
