5da3d9ff816c1a916b0951bf2a57c858b8a38dc8
Megan McCarty <megan_mccarty@hotmail.com>
2021-09-06 12:27:44 -0400

    Add route for taxons

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
     resources :records, only: [:index, :create, :update, :destroy]
+    resources :taxons, only: [:index]
     resources :users, only: [:create]
 end
