443fb70098daa7c4498e8797795b6760be452e00
Megan McCarty <megan_mccarty@hotmail.com>
2021-09-07 11:00:16 -0400

    Get cookies/sessions to work

--- config/application.rb
+++ config/application.rb
@@ -20,21 +20,35 @@ require "rails/test_unit/railtie"
 Bundler.require(*Rails.groups)
 
 module LifelistApp
-  class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.1
+    class Application < Rails::Application
+        # Initialize configuration defaults for originally generated Rails version.
+        config.load_defaults 6.1
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+        config.middleware.use ActionDispatch::Cookies
+        config.middleware.use ActionDispatch::Session::CookieStore
+        config.action_dispatch.cookies_same_site_protection = :strict
 
-    # Only loads a smaller set of middleware suitable for API only apps.
-    # Middleware like session, flash, cookies can be added back manually.
-    # Skip views, helpers and assets when generating a new resource.
-    config.api_only = true
-  end
+        # Configuration for the application, engines, and railties goes here.
+        #
+        # These settings can be overridden in specific environments using the files
+        # in config/environments, which are processed later.
+        #
+        # config.time_zone = "Central Time (US & Canada)"
+        # config.eager_load_paths << Rails.root.join("extras")
+
+        # Only loads a smaller set of middleware suitable for API only apps.
+        # Middleware like session, flash, cookies can be added back manually.
+        # Skip views, helpers and assets when generating a new resource.
+        config.api_only = true
+
+        config.middleware.insert_before 0, Rack::Cors do
+            allow do
+                origins 'http://localhost:3001'
+                resource '*',
+                    headers: :any,
+                    methods: [:get, :post, :put, :patch, :delete, :options, :head],
+                    credentials: true
+            end
+        end
+    end
 end
