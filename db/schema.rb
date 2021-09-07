18884aee113a5d8643eec87e4b48fd927f84abd1
Megan McCarty <megan_mccarty@hotmail.com>
2021-09-07 13:47:12 -0400

    Add category field to records table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_07_162924) do
+ActiveRecord::Schema.define(version: 2021_09_07_174613) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2021_09_07_162924) do
     t.integer "taxon_id"
     t.string "notes"
     t.integer "user_id"
+    t.string "category"
     t.index ["taxon_id"], name: "index_records_on_taxon_id"
     t.index ["user_id"], name: "index_records_on_user_id"
   end
