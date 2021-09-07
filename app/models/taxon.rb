e17fae10ba1d3aa3f30bbcbe05f1473e1666883d
Megan McCarty <megan_mccarty@hotmail.com>
2021-09-07 11:03:20 -0400

    Add image key to custom method

--- app/models/taxon.rb
+++ app/models/taxon.rb
@@ -6,7 +6,8 @@ class Taxon < ApplicationRecord
         def taxon_from_api(result)
             self.find_or_create_by(
                 species: result[0].name,
-                common_name: result[0].preferred_common_name
+                common_name: result[0].preferred_common_name,
+                image: result[0].default_photo.medium_url
             ) 
         end
     end
