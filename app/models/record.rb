c7e3970344a739f73808769b700ba07a143ad1e4
Megan McCarty <megan_mccarty@hotmail.com>
2021-09-08 11:29:12 -0400

    Add model validations

--- app/models/record.rb
+++ app/models/record.rb
@@ -1,4 +1,5 @@
 class Record < ApplicationRecord
+    validates :taxon_id, uniqueness: { scope: :user_id, message: 'already exists in your life list!' }
     belongs_to :user
     belongs_to :taxon
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
+    validates :username, presence: true
+    validates :username, uniqueness: true
+    
     has_secure_password
+
     has_many :records
     has_many :taxons, through: :records
 end
