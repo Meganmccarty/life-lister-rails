f4d50674684aa28ad9c59e2bc901d402b6b83506
Megan McCarty <megan_mccarty@hotmail.com>
2021-09-09 16:15:25 -0400

    Change routes for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
     resources :records, only: [:index, :create, :update, :destroy]
     resources :taxons, only: [:index]
+    resources :users, only: [:update]
+    get "/users/:username", to: "users#show"
     post "/signup", to: "users#create"
-    get "/profile", to: "users#show"
+    get "/profile", to: "users#display"
     post "/login", to: "sessions#create"
     delete "logout", to: "sessions#destroy"
 end
