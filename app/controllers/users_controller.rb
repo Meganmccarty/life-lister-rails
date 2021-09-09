babebbcaac1d04e5120e5e2b3b9279a520d13ac3
Megan McCarty <megan_mccarty@hotmail.com>
2021-09-09 16:16:53 -0400

    Rename show method to display, create new show and update methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-    before_action :authorize, only: [:show]
+    before_action :authorize, only: [:display]
     rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
     def create
@@ -11,6 +11,17 @@ class UsersController < ApplicationController
     end
 
     def show
+        user = User.find_by(username: params[:username])
+        render json: user, include: ['records', 'records.taxon']
+    end
+
+    def update
+        user = User.find(params[:id])
+        user.update(user_params)
+        render json: user
+    end
+
+    def display
         user = User.find_by(id: session[:user_id])
         render json: user
     end
@@ -18,7 +29,7 @@ class UsersController < ApplicationController
     private
 
     def user_params
-        params.permit(:username, :password, :password_confirmation)
+        params.permit(:username, :password, :password_confirmation, :public_profile)
     end
 
     def authorize
