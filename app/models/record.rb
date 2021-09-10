ab4c8ed7f97168301753841da0c107d06e2b8c38
Megan McCarty <megan_mccarty@hotmail.com>
2021-09-10 12:29:10 -0400

    Add validation for category

--- app/models/record.rb
+++ app/models/record.rb
@@ -1,5 +1,9 @@
 class Record < ApplicationRecord
     validates :taxon_id, uniqueness: { scope: :user_id, message: 'already exists in your life list!' }
+    validates :category, inclusion: { 
+        in: ['Amphibian', 'Bird', 'Butterfly', 'Fish', 'Insect', 'Mammal', 'Plant', 'Reptile', 'Other'],
+        message: 'is not a valid category'
+    }
     belongs_to :user
     belongs_to :taxon
 end
