709a5fe3ea05809665b7460c7f0f8d8a13950699
Megan McCarty <megan_mccarty@hotmail.com>
2021-09-13 10:30:37 -0400

    Set response headers content type to application/json

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::API
     include ActionController::Cookies
+    before_action :set_headers
+
+    def set_headers
+        response.headers['Content-Type'] = 'application/vnd.api+json'
+    end
 end
