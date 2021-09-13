1423594c1ce11dcf8f56dde2b6970810b2282cb1
Megan McCarty <megan_mccarty@hotmail.com>
2021-09-13 11:09:38 -0400

    Modify session_store.rb

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,5 @@
-Rails.application.config.session_store :cookie_store, same_site: :none, secure: true
+if Rails.env == 'production' 
+    Rails.application.config.session_store :cookie_store, key: '_lifelist_app_session', same_site: :none, secure: true, domain: 'frozen-hollows-38826.herokuapp.com'
+else
+    Rails.application.config.session_store :cookie_store, key: '_myapp' 
+end
