6ae5868e506edf59f090270c76a3b54ed4db2695
Megan McCarty <megan_mccarty@hotmail.com>
2021-09-17 16:16:25 -0400

    Move up path route to top

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,15 @@ Rails.application.routes.draw do
     resources :records, only: [:index, :create, :update, :destroy]
     resources :taxons, only: [:index]
     resources :users, only: [:update]
+
+    get '*path', to: "application#fallback_index_html", constraints: ->(request) do
+        !request.xhr? && request.format.html?
+    end
+
     get "/users/:username", to: "users#show"
     post "/signup", to: "users#create"
     get "/profile", to: "users#display"
     post "/login", to: "sessions#create"
     delete "logout", to: "sessions#destroy"
     # get '*other', to: 'welcome#index'
-
-    get '*path', to: "application#fallback_index_html", constraints: ->(request) do
-        !request.xhr? && request.format.html?
-    end
 end
