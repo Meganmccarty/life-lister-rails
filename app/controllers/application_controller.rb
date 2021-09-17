ccab9300bd4c53263435c4d1e1ea9105f1556802
Megan McCarty <megan_mccarty@hotmail.com>
2021-09-17 18:49:29 -0400

    Add prepend: true to protect_from_forgery

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-    protect_from_forgery with: :exception
+    protect_from_forgery with: :exception, prepend: true
     include ActionController::Cookies
 
     def fallback_index_html
