2dfd5548bfa2d128afacdcbc1469ae509ffdb5a8
Megan McCarty <megan_mccarty@hotmail.com>
2021-09-17 19:02:38 -0400

    Test if protect_from_forgery with: :null_session will work

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-    protect_from_forgery with: :exception, prepend: true
+    protect_from_forgery with: :null_session
     include ActionController::Cookies
 
     def fallback_index_html
