ea4b511198da7a8b1e68205a68600ef2e2db1927
Megan McCarty <megan_mccarty@hotmail.com>
2021-09-18 20:11:54 -0400

    Add custom method for setting csrf token in a cookie

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,16 @@
 class ApplicationController < ActionController::Base
     include ActionController::Cookies
-    skip_before_action :verify_authenticity_token
+    # skip_before_action :verify_authenticity_token
     # protect_from_forgery with: :exception, prepend: true
+    before_action :set_csrf_cookie
 
     def fallback_index_html
         render file: 'public/index.html'
+    end
+
+    private
+
+    def set_csrf_cookie
+        cookies["CSRF-TOKEN"] = form_authenticity_token
     end
 end
