6a1c95a9b52fa8aabde67c9404a0e104e22b653c
Megan McCarty <megan_mccarty@hotmail.com>
2021-09-20 15:55:21 -0400

    Remove unnecessary file

--- config/initializers/session_store.rb
+++
@@ -1,8 +0,0 @@
-if Rails.env == 'production'
-    Rails.application.config.session_store(:cookie_store, {
-        key: '_session_id',
-        same_site: :none,
-        secure: true,
-        domain: 'frozen-hollows-38826.herokuapp.com'
-    })
-end
